86c899ea2b088ae49d03f9267e087666efe7fb08
cristianrm24 <soliscris72@gmail.com>
2023-11-25 22:29:45 -0600

    actualizacion de datos y add de .java

--- AnalSQL/src/main/java/com/mycompany/analsql/ASLR.java
+++ AnalSQL/src/main/java/com/mycompany/analsql/ASLR.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.mycompany.analsql;
 import java.util.List;
 import java.util.Stack;
@@ -46,10 +45,14 @@ public class ASLR {
                }
                 
             }else if(ap.equals("P")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
+                if(ap=="IDENTIFICADOR"){
+                    pila.pop();
+                    pila.push("A");
+                    ap=pila.peek();
+                }else if(ap=="ASTERISCO"){
+                    pila.pop();
+                    pila.push("ASTERISCO");
+                    ap=pila.peek();
                 }
                 else{
                 System.out.println("Error, no cumples con als condiciones de la tabla");
@@ -58,9 +61,11 @@ public class ASLR {
                }
             }else if(ap.equals("A")){
                 if(ap==""){
+                    pila.pop();
+                    pila.push("A1");
+                    pila.push("A2");
                     
-                }else if(ap==""){
-                    
+                    ap=pila.peek();
                 }
                 else{
                 System.out.println("Error, no cumples con als condiciones de la tabla");
@@ -68,10 +73,14 @@ public class ASLR {
                 break;
                }
             }else if(ap.equals("A1")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
+                if(ap=="FROM"){
+                    pila.pop();
+                    ap=pila.peek();
+                }else if(ap=="COMA"){
+                    pila.pop();
+                    pila.push("A");
+                    pila.push("COMA");
+                    ap=pila.peek();
                 }
                 else{
                 System.out.println("Error, no cumples con als condiciones de la tabla");
@@ -79,21 +88,33 @@ public class ASLR {
                 break;
                }
             }else if(ap.equals("A2")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
-                }
-                else{
+                if(ap=="IDENTIFICADOR"){
+                    pila.pop();
+                    pila.push("A3");
+                    pila.push("IDENTIFICADOR");
+                    ap=pila.peek();
+                }else{
                 System.out.println("Error, no cumples con als condiciones de la tabla");
                 Error = true; // Marca que se ha encontrado un error
                 break;
                }
             }else if(ap.equals("A3")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
+                if(ap=="AOF"){
+                    pila.pop();
+                    ap=pila.peek();
+                }else if(ap=="FROM"){
+                    pila.pop();
+                    ap=pila.peek();
+                }
+                else if(ap=="PUNTO"){
+                    pila.pop();
+                    pila.push("ID");
+                    pila.push("PUNTO");
+                    ap=pila.peek();
+                }
+                else if(ap=="COMA"){
+                    pila.pop();
+                    ap=pila.peek();
                 }
                 else{
                 System.out.println("Error, no cumples con als condiciones de la tabla");
@@ -101,21 +122,25 @@ public class ASLR {
                 break;
                }
             }else if(ap.equals("T")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
-                }
-                else{
+                if(ap=="IDENTIFICADOR"){
+                    pila.pop();
+                    pila.push("T1");
+                    pila.push("T2");
+                    ap=pila.peek();
+                }else{
                 System.out.println("Error, no cumples con als condiciones de la tabla");
                 Error = true; // Marca que se ha encontrado un error
                 break;
                }
             }else if(ap.equals("T1")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
+                if(ap=="AOF"){
+                    pila.pop();
+                    ap=pila.peek();
+                }else if(ap=="COMA"){
+                    pila.pop();
+                    pila.push("T");
+                    pila.push("COMA");
+                    ap=pila.peek();
                 }
                 else{
                 System.out.println("Error, no cumples con als condiciones de la tabla");
@@ -123,23 +148,29 @@ public class ASLR {
                 break;
                }
             }else if(ap.equals("T2")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
-                }
-                else{
+                if(ap=="IDENTIFICADOR"){
+                    pila.pop();
+                    pila.push("T3");
+                    pila.push("IDENTIFICADOR");
+                    ap=pila.peek();
+                }else{
                 System.out.println("Error, no cumples con als condiciones de la tabla");
                 Error = true; // Marca que se ha encontrado un error
                 break;
                }
             }else if(ap.equals("T3")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
+                if(ap=="IDENTIFICADOR"){
+                    pila.pop();
+                    pila.push("IDENTIFICADOR");
+                    ap=pila.peek();
+                }else if(ap=="AOF"){
+                    pila.pop();
+                    ap=pila.peek();
                 }
-                else{
+                else if(ap=="COMA"){
+                    pila.pop();
+                    ap=pila.peek();
+                }else{
                 System.out.println("Error, no cumples con als condiciones de la tabla");
                 Error = true; // Marca que se ha encontrado un error
                 break;
@@ -151,157 +182,3 @@ public class ASLR {
     
     
 }
-=======
-package com.mycompany.analsql;
-import java.util.List;
-import java.util.Stack;
-public class ASLR {
-    
-    public boolean Error=false;
-    
-    public int i=0;//contador de nuestra cadena en SQL
-    
-    
-    public void tabla(){
-        Stack<String> pila = new Stack<>();//crea pila
-        pila.push("EOF");
-        pila.push("Q");//Inicializamos la pila con los valores $,Q
-        
-        String ap=pila.peek();//asignamos al apuntador al tope de la pila
-        
-        while(!"EOF".equals(ap)){//mientras la pila sea diferente del elemento vacio procedemos con la logica
-            //para llenar el bucle hacemos uso de nuestra tabla previamente creada
-            if(ap.equals("Q")){
-                //intercambiamos por su equivalente
-                pila.pop();
-                pila.push("T");
-                pila.push("FROM");
-                pila.push("D");
-                pila.push("SELECT");
-                ap=pila.peek();
-            }else if(ap.equals("D")){//A diferencia de la Q, esta consta de 3 casos posibles
-                                     //por ende aplicamos 3 ifs.
-                if(ap=="DISTINCT"){
-                    pila.pop();
-                    pila.push("P");
-                    pila.push("DISTINCT");
-                    ap=pila.peek();
-               }
-               else if(ap=="IDENTIFICADOR"||ap=="ASTERISCO"){
-                    pila.pop();
-                    pila.push("P");
-                    ap=pila.peek();
-               }
-               else{
-                System.out.println("Error, no cumples con als condiciones de la tabla");
-                Error = true; // Marca que se ha encontrado un error
-                break;
-               }
-                
-            }else if(ap.equals("P")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
-                }
-                else{
-                System.out.println("Error, no cumples con als condiciones de la tabla");
-                Error = true; // Marca que se ha encontrado un error
-                break;
-               }
-            }else if(ap.equals("A")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
-                }
-                else{
-                System.out.println("Error, no cumples con als condiciones de la tabla");
-                Error = true; // Marca que se ha encontrado un error
-                break;
-               }
-            }else if(ap.equals("A1")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
-                }
-                else{
-                System.out.println("Error, no cumples con als condiciones de la tabla");
-                Error = true; // Marca que se ha encontrado un error
-                break;
-               }
-            }else if(ap.equals("A2")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
-                }
-                else{
-                System.out.println("Error, no cumples con als condiciones de la tabla");
-                Error = true; // Marca que se ha encontrado un error
-                break;
-               }
-            }else if(ap.equals("A3")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
-                }
-                else{
-                System.out.println("Error, no cumples con als condiciones de la tabla");
-                Error = true; // Marca que se ha encontrado un error
-                break;
-               }
-            }else if(ap.equals("T")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
-                }
-                else{
-                System.out.println("Error, no cumples con als condiciones de la tabla");
-                Error = true; // Marca que se ha encontrado un error
-                break;
-               }
-            }else if(ap.equals("T1")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
-                }
-                else{
-                System.out.println("Error, no cumples con als condiciones de la tabla");
-                Error = true; // Marca que se ha encontrado un error
-                break;
-               }
-            }else if(ap.equals("T2")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
-                }
-                else{
-                System.out.println("Error, no cumples con als condiciones de la tabla");
-                Error = true; // Marca que se ha encontrado un error
-                break;
-               }
-            }else if(ap.equals("T3")){
-                if(ap==""){
-                    
-                }else if(ap==""){
-                    
-                }
-                else{
-                System.out.println("Error, no cumples con als condiciones de la tabla");
-                Error = true; // Marca que se ha encontrado un error
-                break;
-               }
-            }
-                
-        }
-    }
-    
-    
-}
->>>>>>> 8631d0def4be6b26699a9653fbed0358d40ad3b1
